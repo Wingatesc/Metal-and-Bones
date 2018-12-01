1a8eb1e0606cacad02810ae21b15fee30e3a9313
Eyon <patrick.eyon.aho@gmail.com>
2018-12-01 14:37:57 +0100

    tuto level color new

---
+++ Beat_them_all_1/Assets/Scripts/Autre/openOrClosePanel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openOrClosePanel : MonoBehaviour {
+    public bool open;
+    public GameObject fenetre;
+    public void OnClick()
+    {
+        if (open&&fenetre!=null)
+        {
+            fenetre.SetActive(true);
+        }
+        if (!open && fenetre != null) //when I want to close a panel
+        {
+             fenetre.SetActive(false);
+        }
+    }
+}
