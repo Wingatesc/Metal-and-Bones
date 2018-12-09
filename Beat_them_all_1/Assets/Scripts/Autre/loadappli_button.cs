5b13bb5842c6cd5f2c3e42435cc2661569eda885
Eyon <patrick.eyon.aho@gmail.com>
2018-12-09 22:01:46 +0100

    build 3.5.1
    toujours le même build, je l'ai présenté au toatsbuds

--- Beat_them_all_1/Assets/Scripts/Autre/loadappli_button.cs
+++ Beat_them_all_1/Assets/Scripts/Autre/loadappli_button.cs
@@ -4,11 +4,12 @@ using UnityEngine.SceneManagement;
 using UnityEditor;
 
 public class loadappli_button : MonoBehaviour {
-    public SceneAsset Scene;
-	// Use this for initialization
 
-	public void OnClick(){
-         SceneManager.LoadScene(Scene.name);
+    //public SceneAsset Scene;
+    public string levelname;
+
+    public void OnClick(){
+        SceneManager.LoadScene(levelname, LoadSceneMode.Single);
 
     }
 
